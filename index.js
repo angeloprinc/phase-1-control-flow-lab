2ad098fbd264ec1a634a5014f358e0e3c24ae932
ANGELO PRINCE <angelo@angelo.com>
2024-03-20 00:04:15 +0300

    control flow lab

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+  if(ride <= 400){
+    "give free sample"
+    else if
+  }
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
+  const city = "NYC"
+  city = city? ("ok, sounds good."
 }
 
 function switchOnCharmFromTip(){
